90db8abcb12d422584368d3a2370a4cd95c27ec1
knoffelcut <hardus_richter@hotmail.com>
2021-07-19 16:27:22 +0200

    Add tests for City of Cape Town

--- test_loadshedding.py
+++ test_loadshedding.py
@@ -87,7 +87,65 @@ test_areas = {
             ('1B', '2021-02-28 23:35:30', 5, False),
             ('1B', '2021-02-28 23:50:36', 5, True),
         )
-    }
+    },
+    "city_of_cape_town": {
+        'configuration_system': {
+            'API_URL':
+                'http://loadshedding.eskom.co.za/LoadShedding/GetStatus',
+            'LOGSTAGE': 'stagelog.txt',
+            'LOG': 'log.log',
+            'MIN_OFFSET': 4,
+            'MAX_OFFSET': 17,
+            'NOTIFICATION_TIMEOUT': 0
+        },
+        'configuration_user': {
+            'AREA': 3,
+            'SCHEDULE_CSV': 'schedules/load_shedding_city_of_cape_town.csv',
+            'CMD': 'sudo /usr/sbin/s2disk',
+            'GTK_NOTIFICATION': False
+        },
+        'test_select': (
+            # On-Off at between stage 1 and 2
+            (1, '2021-07-13 11:28:30', 1, False),
+            (1, '2021-07-13 11:28:30', 2, True),
+            # On-Off Before loadshedding starts
+            (9, '2020-01-01 13:55:03', 1, False),
+            (9, '2020-01-01 13:55:03', 4, False),
+            (9, '2020-01-01 13:55:03', 5, True),
+            (9, '2020-01-01 15:55:03', 8, True),
+            # Off before loadshedding stops
+            (15, '2025-12-21 20:26:00', 4, True),
+            (15, '2025-12-21 20:26:59', 4, True),
+            (15, '2025-12-21 20:27:00', 4, False),
+            (15, '2025-12-21 20:35:00', 4, False),
+            # On before starts
+            (11, '2030-08-09 15:42:30', 1, False),
+            (11, '2030-08-09 15:42:59', 1, False),
+            (11, '2030-08-09 15:43:00', 1, True),
+            (11, '2030-08-09 15:43:14', 1, True),
+            (11, '2030-08-09 16:05:11', 1, True),
+        ),
+        'test_midnight_tomorrow': (
+            (13, '2021-07-01 23:35:30', 1, False),
+            (13, '2021-07-01 23:50:36', 1, True),
+
+            (4, '2021-07-13 23:35:30', 4, False),
+            (4, '2021-07-13 23:58:30', 3, False),
+            (4, '2021-07-13 23:58:30', 4, True),
+        ),
+        'test_early_morning_yesterday': (
+            (13, '2021-07-06 00:02:33', 1, True),
+            (13, '2021-07-06 00:29:12', 1, False),
+        ),
+        'test_midnight_month': (
+            # 30 November for shedding on 01 December
+            (13, '2021-11-30 23:35:30', 5, False),
+            (13, '2021-11-30 23:50:36', 5, True),
+            # 28 Feb 2021 shedding for 01 March
+            (9, '2021-02-28 23:35:30', 5, False),
+            (9, '2021-02-28 23:50:36', 5, True),
+        )
+    },
 }
 
 
